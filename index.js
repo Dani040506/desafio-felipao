f23582767a8f5b77b1b2fdd1dccfdd61b6a1cde8
Danielle <dniellebraganca9@gmail.com>
2025-07-18 21:14:51 -0300

    Desafio do Felipão da DIO

--- index.js
+++ index.js
@@ -3,23 +3,23 @@
 let nome = "Dani"
 let xpnumerica = 0
 let nivelHeroi = ""
-if(xp <= 1000){
-    xp = "Ferro"
-}else if(xpnumerica >= 1001 && xp <= 2000){
+if(xpnumerica <= 1000){
+    nivelHeroi = "Ferro"
+}else if(xpnumerica >= 1001 && xpnumerica <= 2000){
     nivelHeroi = "Bronze"
-}else if(xpnumerica >= 2001 && xp <= 5000){
+}else if(xpnumerica >= 2001 && xpnumerica <= 5000){
     nivelHeroi = "Prata"
-}else if(xpnumerica >= 5001 && xp <= 7000){
+}else if(xpnumerica >= 5001 && xpnumerica <= 7000){
     nivelHeroi = "Ouro"
-}else if(xpnumerica >= 7001 && xp <= 8000){
+}else if(xpnumerica >= 7001 && xpnumerica <= 8000){
     nivelHeroi = "Platina"
-}else if(xpnumerica >= 8001 && xp <= 9000){
+}else if(xpnumerica >= 8001 && xpnumerica <= 9000){
     nivelHeroi = "Ascendente"
-}else if(xpnumerica >= 9001 && xp <= 10000){
+}else if(xpnumerica >= 9001 && xpnumerica <= 10000){
     nivelHeroi = "Imortal"
 }else if(xpnumerica >= 10001){
     nivelHeroi = "Radiante"
 }
 
 
-console.log("O herói de nome" + nome + "está no nível de " + nivelHeroi)
+console.log("O herói de nome " + nome + " está no nível de " + nivelHeroi)
